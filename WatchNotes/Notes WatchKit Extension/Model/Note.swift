bc8815066146afdf19a8d3146252e590ea14e1fa
Yaşar Duman <01.yasarduman@gmail.com>
2023-10-01 13:19:19 +0300

    WatchNotes

---
+++ WatchNotes/Notes WatchKit Extension/Model/Note.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Yaşar Duman on 27.09.2023.
+//
+
+import Foundation
+
+struct Note: Identifiable, Codable {
+  let id: UUID
+  let text: String
+}
---
+++ WatchNotes/Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Yaşar Duman on 27.09.2023.
+//
+
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
